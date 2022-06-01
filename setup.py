91f12ba68f5c1ed86600c7f7fd5016d2cb9ecea2
Jeremy Steele <jeremy.ma.steele@gmail.com>
2022-06-01 15:02:59 -0400

    bump: version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gtfs-fares-v2-validator',
-    version='0.1.0',
+    version='1.0.0',
     description=
     'Validate transit feeds for conformance to the GTFS Fares v2 specification',
     url='https://github.com/TransitApp/gtfs-fares-v2-validator',
